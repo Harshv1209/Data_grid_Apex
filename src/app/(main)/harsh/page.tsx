c1b0a10903871f2eb1bbca61ba64e8f04d683f6a
Harshv1209 <1320204019@bitraipur.ac.in>
2023-09-22 13:29:49 +0530

    added data grid

--- src/app/(main)/harsh/page.tsx
+++ src/app/(main)/harsh/page.tsx
@@ -1,7 +1,23 @@
-import React from "react"
+"use client"
+import * as React from "react"
+import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid"
+import { Stack } from "@mui/material"
 
-function page() {
-  return <div>Harsh vaidya</div>
+const rows: GridRowsProp = [
+  { id: 1, col1: "Hello", col2: "World" },
+  { id: 2, col1: "DataGridPro", col2: "is Awesome" },
+  { id: 3, col1: "MUI", col2: "is Amazing" },
+]
+
+const columns: GridColDef[] = [
+  { field: "col1", headerName: "Column 1", width: 150 },
+  { field: "col2", headerName: "Column 2", width: 150 },
+]
+
+export default function App() {
+  return (
+    <Stack sx={{ color: "white", bgcolor: "grey" }}>
+      <DataGrid rows={rows} columns={columns} />
+    </Stack>
+  )
 }
-
-export default page
